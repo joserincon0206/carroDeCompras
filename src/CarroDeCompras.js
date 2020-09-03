8ca84e904a0ab4612b4d3a965ba859d4c20e0b56
joserincon0206 <38759680+joserincon0206@users.noreply.github.com>
2020-09-02 20:51:25 -0400

    Initial commit

---
+++ src/CarroDeCompras.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="CarroDeCompras">
+      <h1>Hola soy un carro de compras</h1>
+      <h2>Compra algo no seas tacano</h2>
+    </div>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+import CarroDeCompras from "./CarroDeCompras";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <div>
+      <App />
+      <CarroDeCompras />
+    </div>
+  </React.StrictMode>,
+  rootElement
+);
